40c6e99c62b8066eecb46c3155a7d50aa54641ab
zhangzhen <zhangzhen@oneweone.cn>
2020-09-13 09:51:12 +0800

    对接寝趣

--- app/Http/Controllers/OrderMinKangController.php
+++ app/Http/Controllers/OrderMinKangController.php
@@ -61,8 +61,8 @@ class OrderMinKangController extends Controller
     public function get_cancel(Request $request)
     {
         if ($request->get('order_id')) {
-            // \Log::info('民康订单-取消',[$request->all()]);
-            // file_get_contents('http://psapi.625buy.com/api/order/cancel?type=4&order_id='.$request->get('order_id'));
+            \Log::info('民康订单-取消',[$request->all()]);
+            file_get_contents('http://psapi.625buy.com/api/order/cancel?type=4&order_id='.$request->get('order_id'));
             return json_encode(['data' => 'ok']);
         }
         return 200;
