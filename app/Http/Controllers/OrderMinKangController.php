98cd669b1434c06edce1282b0286779502c4fa07
zhangzhen <zhangzhen@yeah.net>
2020-12-19 16:01:29 +0800

    对接寝趣

--- app/Http/Controllers/OrderMinKangController.php
+++ app/Http/Controllers/OrderMinKangController.php
@@ -42,54 +42,17 @@ class OrderMinKangController extends Controller
                 "order_tag_list" => $request->get("order_tag_list"),
                 "wm_order_id_view" => $request->get("wm_order_id_view"),
                 "app_poi_code" => $request->get("app_poi_code"),
-                "wm_poi_name" => $request->get("wm_poi_name"),
-                "wm_poi_address" => $request->get("wm_poi_address"),
-                "wm_poi_phone" => $request->get("wm_poi_phone"),
-                "recipient_address" => $request->get("recipient_address"),
-                "recipient_phone" => $request->get("recipient_phone"),
-                "backup_recipient_phone" => $request->get("backup_recipient_phone"),
-                "recipient_name" => $request->get("recipient_name"),
-                "shipping_fee" => $request->get("shipping_fee"),
-                "total" => $request->get("total"),
-                "original_price" => $request->get("original_price"),
-                "caution" => $request->get("caution"),
-                "shipper_phone" => $request->get("shipper_phone"),
-                "status" => $request->get("status"),
-                "ctime" => $request->get("ctime"),
-                "utime" => $request->get("utime"),
-                "delivery_time" => $request->get("delivery_time"),
-                "is_third_shipping" => $request->get("is_third_shipping"),
-                "pick_type" => $request->get("pick_type"),
-                "latitude" => $request->get("latitude"),
-                "longitude" => $request->get("longitude"),
-                "day_seq" => $request->get("day_seq"),
-                "logistics_code" => $request->get("logistics_code"),
-                "poi_receive_detail" => $request->get("poi_receive_detail"),
-                "detail" => $request->get("detail"),
-                "extras" => $request->get("extras"),
-                "sku_benefit_detail" => $request->get("sku_benefit_detail"),
-                "package_bag_money" => $request->get("package_bag_money"),
-                "package_bag_money_yuan" => $request->get("package_bag_money_yuan"),
-                "poi_receive_detail_yuan" => $request->get("poi_receive_detail_yuan"),
-                "total_weight" => $request->get("total_weight"),
-            ];
-            \Log::info('民康-推送已完成订单', $order);
-            $order2 = [
-                "order_id" => $request->get("order_id"),
-                "order_tag_list" => $request->get("order_tag_list"),
-                "wm_order_id_view" => $request->get("wm_order_id_view"),
-                "app_poi_code" => $request->get("app_poi_code"),
                 "wm_poi_name" => urldecode($request->get("wm_poi_name")),
                 "wm_poi_address" => urldecode($request->get("wm_poi_address")),
                 "wm_poi_phone" => $request->get("wm_poi_phone"),
-                "recipient_address" => $request->get("recipient_address"),
+                "recipient_address" => urldecode($request->get("recipient_address")),
                 "recipient_phone" => $request->get("recipient_phone"),
-                "backup_recipient_phone" => $request->get("backup_recipient_phone"),
-                "recipient_name" => $request->get("recipient_name"),
+                "backup_recipient_phone" => urldecode($request->get("backup_recipient_phone")),
+                "recipient_name" => urldecode($request->get("recipient_name")),
                 "shipping_fee" => $request->get("shipping_fee"),
                 "total" => $request->get("total"),
                 "original_price" => $request->get("original_price"),
-                "caution" => $request->get("caution"),
+                "caution" => urldecode($request->get("caution")),
                 "shipper_phone" => $request->get("shipper_phone"),
                 "status" => $request->get("status"),
                 "ctime" => $request->get("ctime"),
@@ -101,16 +64,32 @@ class OrderMinKangController extends Controller
                 "longitude" => $request->get("longitude"),
                 "day_seq" => $request->get("day_seq"),
                 "logistics_code" => $request->get("logistics_code"),
-                "poi_receive_detail" => $request->get("poi_receive_detail"),
-                "detail" => $request->get("detail"),
-                "extras" => $request->get("extras"),
-                "sku_benefit_detail" => $request->get("sku_benefit_detail"),
                 "package_bag_money" => $request->get("package_bag_money"),
                 "package_bag_money_yuan" => $request->get("package_bag_money_yuan"),
-                "poi_receive_detail_yuan" => $request->get("poi_receive_detail_yuan"),
                 "total_weight" => $request->get("total_weight"),
             ];
-            \Log::info('民康-推送已完成订单', $order2);
+            \Log::info('民康-推送已完成订单', $order);
+
+            // 商家对账
+            $poi_receive_detail_yuan = $request->get("poi_receive_detail_yuan");
+            if ($poi_receive_detail_yuan) {
+                $duizhang = json_decode(urldecode($poi_receive_detail_yuan));
+                if (!empty($duizhang)) {
+                    \Log::info('民康-完成订单-对账列表', $duizhang);
+                }
+            }
+            // 订单优惠信息
+            // $extras = $request->get("extras");
+            // 商品优惠信息
+            // $sku_benefit_detail = $request->get("sku_benefit_detail");
+            // 订单商品信息
+            $detail = $request->get("detail");
+            if ($detail) {
+                $products = json_decode(urldecode($detail));
+                if (!empty($products)) {
+                    \Log::info('民康-完成订单-商品列表', $products);
+                }
+            }
             return json_encode(['data' => 'ok']);
         }
         return 200;
