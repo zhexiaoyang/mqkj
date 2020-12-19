62c04272bbc5eef3e3a71c80ae3c0b7f6dde504c
zhangzhen <zhangzhen@yeah.net>
2020-12-19 18:35:13 +0800

    创建民康订单

--- app/Http/Controllers/OrderMinKangController.php
+++ app/Http/Controllers/OrderMinKangController.php
@@ -117,6 +117,7 @@ class OrderMinKangController extends Controller
                     }
                 }
                 $order->goods_price = $goods_price / 100;
+                $order->save();
                 MtOrderItem::query()->insert($items);
             }
             return json_encode(['data' => 'ok']);
